0c9878a8fb087f866dd06cc724897a8ddeb10825
sndp231998 <sandipchapagain797@gmail.com>
2024-06-12 12:18:15 +0545

    initial

---
+++ src/main/java/com/BigBangCoders_IICQuest/BigBangCodersIicQuestApplication.java
@@ -0,0 +1,13 @@
+package com.BigBangCoders_IICQuest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigBangCodersIicQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigBangCodersIicQuestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BigBangCoders_IICQuest/BigBangCodersIicQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BigBangCoders_IICQuest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigBangCodersIicQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
